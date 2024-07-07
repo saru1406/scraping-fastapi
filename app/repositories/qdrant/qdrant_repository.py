9ba63fd09fce5fad10643b8362e54afa71d3b33c
saru1406 <y.s1406@ezweb.ne.jp>
2024-07-08 01:43:37 +0900

    qdrantコレクション作成メソッド

---
+++ app/repositories/qdrant/qdrant_repository.py
@@ -0,0 +1,19 @@
+import os
+from fastapi import Depends
+from qdrant_client import QdrantClient
+from qdrant_client.models import VectorParams, Distance
+
+def get_qdrant_client() -> QdrantClient:
+    qdrant_url = os.getenv("QDRANT_URL")
+    return QdrantClient(url=qdrant_url)
+
+class QdrantRepository:
+
+    def __init__(self, qdrant_client: QdrantClient = Depends(get_qdrant_client)) -> None:
+        self.client = qdrant_client
+
+    def create_collection(self, collection_name: str, vector_size: int, distance: Distance = Distance.COSINE) -> None:
+        self.client.recreate_collection(
+            collection_name=collection_name,
+            vectors_config=VectorParams(size=vector_size, distance=distance),
+        )
--- app/routers/prompt_router/prompt_router.py
+++ app/routers/prompt_router/prompt_router.py
@@ -3,6 +3,7 @@ import numpy as np
 from fastapi import APIRouter, Depends, HTTPException
 from sentence_transformers import SentenceTransformer
 from sqlalchemy.orm import Session
+from app.repositories.qdrant.qdrant_repository import QdrantRepository
 
 from app.database import get_db
 
@@ -10,15 +11,16 @@ router = APIRouter()
 
 
 @router.post("/prompt", tags=["prompt"])
-def prompt():
+def prompt(qdrant_repository: QdrantRepository = Depends(QdrantRepository)):
     model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
     sentences = ["私は山田太郎です。", "私は田中花子です。", "これはテスト文章です。"]
     sentence_vectors = model.encode(sentences)
     # ベクトルの次元数を取得
-    d = sentence_vectors.shape[1]
+    vector_size = sentence_vectors.shape[1]
 
     # Faissインデックスを作成
-    index = faiss.IndexFlatL2(d)
-    index.add(sentence_vectors)
-    faiss.write_index(index, "faiss_index_ivf.bin")
-    print(d)
+    # index = faiss.IndexFlatL2(vector_size)
+    # index.add(sentence_vectors)
+    # faiss.write_index(index, "faiss_index_ivf.bin")
+
+    qdrant_repository.create_collection('test', vector_size)
